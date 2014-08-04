dd44492e6b9f5199f784c507963a0a71b35afdf0
Steven McIntyre <steven.mcintyre@injixo.com>
2014-08-04 16:40:05 +0100

    Add employees migration

---
+++ db/migrate/20140804153637_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :status
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
